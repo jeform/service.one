db94a178c500086593105e6b8a9f1fc7acb772d6
Jesse Machado <jeform@gmail.com>
2025-03-31 22:30:47 -0300

    Add @Repository annotation to TaskRepository and fix query condition for due date

--- src/main/java/com/exolution/service/tasks/TaskRepository.java
+++ src/main/java/com/exolution/service/tasks/TaskRepository.java
@@ -2,13 +2,13 @@ package com.exolution.service.tasks;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface TaskRepository extends JpaRepository<TasksEntity, Long> {
-
-    @Query("select t from Task t where t.dueDate <: deadline and t.notified = false")
+    @Query("select t from Task t where t.dueDate <= :deadline and t.notified = false")
     List<TasksEntity> findTasksDueWithinDeadLine(LocalDateTime deadline);
-
 }
